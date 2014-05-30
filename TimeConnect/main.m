c062f245ce50639e5f98254144bf3ef712aabdfa
huyle333 <huyle333@yahoo.com>
2014-05-30 10:51:34 -0400

    Initial Commit

---
+++ TimeConnect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimeConnect
+//
+//  Created by Le, Huy on 5/30/14.
+//  Copyright (c) 2014 Cambridge Interns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TimeConnectTests/TimeConnectTests.m
@@ -0,0 +1,34 @@
+//
+//  TimeConnectTests.m
+//  TimeConnectTests
+//
+//  Created by Le, Huy on 5/30/14.
+//  Copyright (c) 2014 Cambridge Interns. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TimeConnectTests : XCTestCase
+
+@end
+
+@implementation TimeConnectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
