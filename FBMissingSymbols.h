c746b5ddd4f826b68986ca499fbf2a2d5ada437b
huyle333 <huyle333@yahoo.com>
2014-06-15 07:29:20 -0400

    Fixed Kevin’s error. iOS linker problems = (

---
+++ FBMissingSymbols.h
@@ -0,0 +1,13 @@
+//
+//  FBMissingSymbols.h
+//  TimeConnect
+//
+//  Created by Le, Huy on 6/15/14.
+//  Copyright (c) 2014 Cambridge Interns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBMissingSymbols : NSObject
+
+@end
---
+++ FBMissingSymbols.m
@@ -0,0 +1,37 @@
+//
+//  FBMissingSymbols.m
+//  TimeConnect
+//
+//  Created by Le, Huy on 6/15/14.
+//  Copyright (c) 2014 Cambridge Interns. All rights reserved.
+//
+
+#import "FBMissingSymbols.h"
+
+@implementation FBMissingSymbols
+
+@end
+
+NSString *FBTokenInformationExpirationDateKey = @"";
+NSString *FBTokenInformationTokenKey = @"";
+NSString *FBTokenInformationUserFBIDKey = @"";
+@interface FBAppCall:NSObject
+@end
+@implementation FBAppCall
+@end
+@interface FBRequest:NSObject
+@end
+@implementation FBRequest
+@end
+@interface FBSession:NSObject
+@end
+@implementation FBSession
+@end
+@interface FBSessionTokenCaching:NSObject
+@end
+@implementation FBSessionTokenCaching
+@end
+@interface FBSessionTokenCachingStrategy:NSObject
+@end
+@implementation FBSessionTokenCachingStrategy
+@end
