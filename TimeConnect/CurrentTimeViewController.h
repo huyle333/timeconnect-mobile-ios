d9220dcee791cf3eb00ca66061a12d0dddb6ba14
Xian Feng <Arion@Xians-MacBook-Pro.local>
2014-06-18 15:22:21 -0400

    Add tab bar color.

---
+++ TimeConnect/CurrentTimeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CurrentTimeViewController.h
+//  TimeConnect
+//
+//  Created by Xian Feng on 6/18/14.
+//  Copyright (c) 2014 Cambridge Interns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CurrentTimeViewController : UIViewController
+
+@end
---
+++ TimeConnect/CurrentTimeViewController.m
@@ -0,0 +1,57 @@
+//
+//  CurrentTimeViewController.m
+//  TimeConnect
+//
+//  Created by Xian Feng on 6/18/14.
+//  Copyright (c) 2014 Cambridge Interns. All rights reserved.
+//
+
+#import "CurrentTimeViewController.h"
+
+@interface CurrentTimeViewController ()
+
+@end
+
+@implementation CurrentTimeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // doing this results in an easier to read unselected state then the default iOS 7 one
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSFontAttributeName : [UIFont fontWithName:@"HelveticaNeue-Bold" size:10.0f],
+                                                        NSForegroundColorAttributeName : [UIColor colorWithRed:.5 green:.5 blue:.5 alpha:1]
+                                                        } forState:UIControlStateNormal];
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSFontAttributeName : [UIFont fontWithName:@"HelveticaNeue-Bold" size:10.0f],
+                                                        NSForegroundColorAttributeName : [UIColor colorWithRed:.0 green:.0 blue:.0 alpha:1]
+                                                        } forState:UIControlStateSelected];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
